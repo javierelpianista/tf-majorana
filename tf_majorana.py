09f3f67144796c90994e90a25bcb92af29b1a7e6
javierelpianista <javier.garcia.tw@hotmail.com>
2021-05-02 14:26:29 -0300

    Corrected error with importing coefficients before setting dps

--- tf_majorana.py
+++ tf_majorana.py
@@ -22,38 +22,11 @@ parser.set_defaults(output_file = 'results.dat')
 
 args = parser.parse_args()
 
-
 # ------------------------------------------------------------------------------
 # Set the number of digits
 # ------------------------------------------------------------------------------
 mp.mp.dps = int(args.digits)
-
-# ------------------------------------------------------------------------------
-# Here we compute the coefficient list. 
-# ------------------------------------------------------------------------------
-
-# The first two coefficients 
-c = [1, 9-mp.sqrt(73)]
-
-# The rest of the coefficients using recurrence Eq. (38) from 
-# Am. J. Phys. 70 (2002) 852-856
-def coeffn(M):
-    global c
-
-    while len(c) < M+1:
-        m = len(c)
-        c0 = 0
-        for n in range(1, m-1):
-            c0 += c[m-n]*(
-                    (n+1)*c[n+1] - 2*(n+4)*c[n] + (n+7)*c[n-1]
-                    )
-        c1 = c[m-1] * ((m+7) - 2*(m+3)*c[1])
-        c2 = c[m-2] * ((m+6)*c[1])
-
-        c.append((c0+c1+c2)/(2*(m+8)-(m+1)*c[1]))
-
-    return c[M]
-
+from coefficients import coeffn
 
 # ------------------------------------------------------------------------------
 # Here we sum the coefficients 
@@ -64,7 +37,7 @@ for n in range(int(args.N) + 1):
     v += coeffn(n)
     # Write the partial sum to the output file every FREQ coefficients
     if n % int(args.print_frequency) == 0:
-        ans = (3/16)**(1/3)*v
+        ans = (mp.mpf(3)/mp.mpf(16))**(1/mp.mpf(3))*v
         write_file = open(args.output_file, 'a')
         write_file.write('{:<6}{}\n'.format(n, ans))
         write_file.close()
